15928f4987ce7924e0fd32e784a0df98da11f0e2
Khalil-BHL <khalil.bahlaouane@gmail.com>
2025-02-07 18:35:47 +0100

    Initial commit - React & Express setup

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Express</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
+
